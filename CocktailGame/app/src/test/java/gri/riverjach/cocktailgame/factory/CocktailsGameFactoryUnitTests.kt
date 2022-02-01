a580eab749e0cc5421aa5d8b2e04f501891ca93d
RiviereGregory <gregory.riviere@laposte.net>
2022-02-01 16:35:13 +0100

    Mockito - stubbing callbacks CocktailsGameFactoryUnitTests

--- CocktailGame/app/src/test/java/gri/riverjach/cocktailgame/factory/CocktailsGameFactoryUnitTests.kt
+++ CocktailGame/app/src/test/java/gri/riverjach/cocktailgame/factory/CocktailsGameFactoryUnitTests.kt
@@ -1,21 +1,48 @@
 package gri.riverjach.cocktailgame.factory
 
+import gri.riverjach.cocktailgame.network.Cocktail
 import gri.riverjach.cocktailgame.repository.CocktailsRepository
+import gri.riverjach.cocktailgame.repository.RepositoryCallback
 import org.junit.Before
 import org.junit.Test
-import org.mockito.kotlin.any
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.verify
+import org.mockito.kotlin.*
 
 class CocktailsGameFactoryUnitTests {
 
     private lateinit var repository: CocktailsRepository
     private lateinit var factory: CocktailsGameFactory
 
+    private val cocktails = listOf(
+        Cocktail("1", "Drink1", "image1"),
+        Cocktail("2", "Drink2", "image2"),
+        Cocktail("3", "Drink3", "image3"),
+        Cocktail("4", "Drink4", "image4")
+    )
+
     @Before
     fun setup() {
         repository = mock()
         factory = CocktailsGameFactoryImpl(repository)
+    }
+
+    @Test
+    fun buildGame_shouldCallOnSuccess() {
+        val callback = mock<CocktailsGameFactory.Callback>()
+        setUpRepositoryWithCocktails(repository)
+
+        factory.buildGame(callback)
+
+        verify(callback).onSuccess(any())
+    }
+
+    private fun setUpRepositoryWithCocktails(
+        repository: CocktailsRepository
+    ) {
+        doAnswer {
+            // 1
+            val callback: RepositoryCallback<List<Cocktail>, String> = it.getArgument(0)
+            callback.onSuccess(cocktails)
+        }.whenever(repository).getAlcoholic(any())
     }
 
     @Test
