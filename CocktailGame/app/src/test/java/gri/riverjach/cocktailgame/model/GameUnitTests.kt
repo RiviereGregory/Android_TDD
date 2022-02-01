38beee3eae46d29fbf20b86d20f42357b05b8277
RiviereGregory <gregory.riviere@laposte.net>
2022-02-01 10:18:59 +0100

    Mockito - Refactoring GameUnitTests et ScoreUnitTests pour les TU

--- CocktailGame/app/src/test/java/gri/riverjach/cocktailgame/model/GameUnitTests.kt
+++ CocktailGame/app/src/test/java/gri/riverjach/cocktailgame/model/GameUnitTests.kt
@@ -8,42 +8,6 @@ import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 
 class GameUnitTests {
-    // 1
-    @Test
-    fun whenIncrementingScore_shouldIncrementCurrentScore() {
-        // 2
-        val game = Game(emptyList(), 0)
-
-        // 3
-        game.incrementScore()
-
-        // 4
-        Assert.assertEquals(
-            "Current score should have been 1",
-            1, game.currentScore
-        )
-
-    }
-
-    @Test
-    fun whenIncrementingScore_aboveHighScore_shouldAlsoIncrementHighScore() {
-        val game = Game(emptyList(), 0)
-
-        game.incrementScore()
-
-        Assert.assertEquals(1, game.highestScore)
-    }
-
-    @Test
-    fun whenIncrementingScore_belowHighScore_shouldNotIncrementHighScore() {
-        val game = Game(emptyList(), 10)
-
-        game.incrementScore()
-
-        Assert.assertEquals(10, game.highestScore)
-    }
-
-
     @Test
     fun whenNextQuestion_readNextQuestion() {
         val questions = listOf(
