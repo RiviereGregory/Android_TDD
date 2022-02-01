e9a789d3d0fef5b0e29d084093be0aef745c8954
RiviereGregory <gregory.riviere@laposte.net>
2022-02-01 17:01:25 +0100

    Mockito - utilisation des annotations

--- CocktailGame/app/src/test/java/gri/riverjach/cocktailgame/RepositoryUnitTests.kt
+++ CocktailGame/app/src/test/java/gri/riverjach/cocktailgame/RepositoryUnitTests.kt
@@ -6,19 +6,26 @@ import gri.riverjach.cocktailgame.repository.CocktailsRepository
 import gri.riverjach.cocktailgame.repository.CocktailsRepositoryImpl
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.*
 
+@RunWith(MockitoJUnitRunner::class)
 class RepositoryUnitTests {
     private lateinit var repository: CocktailsRepository
+
+    @Mock
     private lateinit var api: CocktailsApi
+
+    @Mock
     private lateinit var sharedPreferences: SharedPreferences
+
+    @Mock
     private lateinit var sharedPreferencesEditor: SharedPreferences.Editor
 
     @Before
     fun setup() {
-        api = mock()
-        sharedPreferences = mock()
-        sharedPreferencesEditor = mock()
         whenever(sharedPreferences.edit())
             .thenReturn(sharedPreferencesEditor)
 
