f53d4405f5bdfffa88ad683cee9652c4cb7c5ab5
RiviereGregory <gregory.riviere@laposte.net>
2022-02-01 16:27:33 +0100

    Mockito - saveScore_shouldNotSaveToSharedPreferencesIfLower

--- CocktailGame/app/src/test/java/gri/riverjach/cocktailgame/RepositoryUnitTests.kt
+++ CocktailGame/app/src/test/java/gri/riverjach/cocktailgame/RepositoryUnitTests.kt
@@ -44,4 +44,19 @@ class RepositoryUnitTests {
         verify(sharedPreferences).getInt(any(), any())
     }
 
+    @Test
+    fun saveScore_shouldNotSaveToSharedPreferencesIfLower() {
+        val previouslySavedHighScore = 100
+        val newHighScore = 10
+        val spyRepository = spy(repository)
+        doReturn(previouslySavedHighScore)
+            .whenever(spyRepository)
+            .getHighScore()
+
+        spyRepository.saveHighScore(newHighScore)
+
+        verify(sharedPreferencesEditor, never())
+            .putInt(any(), eq(newHighScore))
+    }
+
 }
