95c3e9cc1720f286120c5e051a027287a14c2b93
RiviereGregory <gregory.riviere@laposte.net>
2022-02-01 16:25:41 +0100

    Mockito - refactor RepositoryUnitTests

--- CocktailGame/app/src/test/java/gri/riverjach/cocktailgame/RepositoryUnitTests.kt
+++ CocktailGame/app/src/test/java/gri/riverjach/cocktailgame/RepositoryUnitTests.kt
@@ -2,48 +2,43 @@ package gri.riverjach.cocktailgame
 
 import android.content.SharedPreferences
 import gri.riverjach.cocktailgame.network.CocktailsApi
+import gri.riverjach.cocktailgame.repository.CocktailsRepository
 import gri.riverjach.cocktailgame.repository.CocktailsRepositoryImpl
+import org.junit.Before
 import org.junit.Test
 import org.mockito.kotlin.*
 
 class RepositoryUnitTests {
+    private lateinit var repository: CocktailsRepository
+    private lateinit var api: CocktailsApi
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var sharedPreferencesEditor: SharedPreferences.Editor
+
+    @Before
+    fun setup() {
+        api = mock()
+        sharedPreferences = mock()
+        sharedPreferencesEditor = mock()
+        whenever(sharedPreferences.edit())
+            .thenReturn(sharedPreferencesEditor)
+
+        repository = CocktailsRepositoryImpl(api, sharedPreferences)
+    }
 
     @Test
     fun saveScore_shouldSaveToSharedPreferences() {
-        val api: CocktailsApi = mock()
-        // 1
-        val sharedPreferencesEditor: SharedPreferences.Editor =
-            mock()
-        val sharedPreferences: SharedPreferences = mock()
-        whenever(sharedPreferences.edit())
-            .thenReturn(sharedPreferencesEditor)
-        val repository = CocktailsRepositoryImpl(
-            api,
-            sharedPreferences
-        )
-
-        // 2
         val score = 100
         repository.saveHighScore(score)
 
-        // 3
         inOrder(sharedPreferencesEditor) {
-            // 4
             verify(sharedPreferencesEditor).putInt(any(), eq(score))
             verify(sharedPreferencesEditor).apply()
         }
     }
 
+
     @Test
     fun getScore_shouldGetFromSharedPreferences() {
-        val api: CocktailsApi = mock()
-        val sharedPreferences: SharedPreferences = mock()
-
-        val repository = CocktailsRepositoryImpl(
-            api,
-            sharedPreferences
-        )
-
         repository.getHighScore()
 
         verify(sharedPreferences).getInt(any(), any())
