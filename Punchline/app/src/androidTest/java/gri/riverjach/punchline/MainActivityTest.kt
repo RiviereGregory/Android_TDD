a441f3fa235f837ae1b84a08f8c227570a7dc1a0
RiviereGregory <gregory.riviere@laposte.net>
2022-03-03 10:37:48 +0100

    User Interface - Setting up the test class

---
+++ Punchline/app/src/androidTest/java/gri/riverjach/punchline/MainActivityTest.kt
@@ -0,0 +1,11 @@
+package gri.riverjach.punchline
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
+import org.koin.test.KoinTest
+
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest : KoinTest {
+
+}
