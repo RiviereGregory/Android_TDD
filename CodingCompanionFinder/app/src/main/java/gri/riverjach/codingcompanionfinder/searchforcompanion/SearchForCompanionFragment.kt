1de44de37775d118efee408fe86da87b0b21a040
RiviereGregory <gregory.riviere@laposte.net>
2022-03-12 16:43:56 +0100

    LEGACY PROJECT - refactor SearchForCompanionFragment.kt

--- CodingCompanionFinder/app/src/main/java/gri/riverjach/codingcompanionfinder/searchforcompanion/SearchForCompanionFragment.kt
+++ CodingCompanionFinder/app/src/main/java/gri/riverjach/codingcompanionfinder/searchforcompanion/SearchForCompanionFragment.kt
@@ -1,56 +1,17 @@
-/*
- * Copyright (c) 2021 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package gri.riverjach.codingcompanionfinder.searchforcompanion
 
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.INVISIBLE
-import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.textfield.TextInputEditText
 import gri.riverjach.codingcompanionfinder.MainActivity
-import gri.riverjach.codingcompanionfinder.R
-import gri.riverjach.codingcompanionfinder.testhooks.IdlingEntity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import org.greenrobot.eventbus.EventBus
+import gri.riverjach.codingcompanionfinder.databinding.FragmentSearchForCompanionBinding
 
 class SearchForCompanionFragment : Fragment() {
 
@@ -62,70 +23,62 @@ class SearchForCompanionFragment : Fragment() {
 
     private lateinit var viewManager: RecyclerView.LayoutManager
 
+    private lateinit var fragmentSearchForCompanionBinding: FragmentSearchForCompanionBinding
+    private lateinit var searchForCompanionViewModel: SearchForCompanionViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_for_companion, container, false)
+        fragmentSearchForCompanionBinding =
+            FragmentSearchForCompanionBinding.inflate(
+                inflater,
+                container, false
+            )
+        searchForCompanionViewModel = ViewModelProvider(this)
+            .get(SearchForCompanionViewModel::class.java)
+        fragmentSearchForCompanionBinding.searchForCompanionViewModel = searchForCompanionViewModel
+        fragmentSearchForCompanionBinding.lifecycleOwner = this
+        return fragmentSearchForCompanionBinding.root
     }
 
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-        view?.findViewById<MaterialButton>(R.id.searchButton)?.setOnClickListener {
+        fragmentSearchForCompanionBinding.searchButton.setOnClickListener {
             try {
                 val inputMethodManager =
                     activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
-                inputMethodManager!!.hideSoftInputFromWindow(activity?.currentFocus?.windowToken, 0)
+                inputMethodManager!!.hideSoftInputFromWindow(
+                    activity?.getCurrentFocus()?.getWindowToken(), 0
+                )
             } catch (e: Exception) {
                 // only happens when the keyboard is already closed
             }
-            searchForCompanions()
+            searchForCompanionViewModel.searchForCompanions()
         }
-
+        setupSearchForCompanions()
         super.onActivityCreated(savedInstanceState)
     }
 
 
-    private fun searchForCompanions() {
-        val companionLocation =
-            view?.findViewById<TextInputEditText>(R.id.searchFieldText)?.text.toString()
-        val noResultsTextView = view?.findViewById<TextView>(R.id.noResults)
-        val searchForCompanionFragment = this
-
-        GlobalScope.launch {
-            accessToken = (activity as MainActivity).accessToken
-            (activity as MainActivity).petFinderService?.let { petFinderService ->
-                // Decrement the idling resources.
-                EventBus.getDefault().post(IdlingEntity(-1))
-                val searchForPetResponse =
-                    petFinderService.getAnimals(accessToken, location = companionLocation)
-                GlobalScope.launch(Dispatchers.Main) {
-                    if (searchForPetResponse.isSuccessful) {
-                        searchForPetResponse.body()?.let {
-
-                            if (it.animals.size > 0) {
-                                noResultsTextView?.visibility = INVISIBLE
-                                viewManager = LinearLayoutManager(context)
-                                companionAdapter =
-                                    CompanionAdapter(it.animals, searchForCompanionFragment)
-                                petRecyclerView = view?.let {
-                                    it.findViewById<RecyclerView>(R.id.petRecyclerView).apply {
-                                        layoutManager = viewManager
-                                        adapter = companionAdapter
-                                    }
-                                }
-                            } else {
-                                noResultsTextView?.visibility = VISIBLE
-                            }
-                        }
-                    } else {
-                        noResultsTextView?.visibility = VISIBLE
-                    }
-                }
-                // Decrement the idling resources.
-                EventBus.getDefault().post(IdlingEntity(-1))
+    private fun setupSearchForCompanions() {
+        searchForCompanionViewModel.accessToken =
+            (activity as MainActivity).accessToken
+        searchForCompanionViewModel.petFinderService =
+            (activity as MainActivity).petFinderService!!
+        viewManager = LinearLayoutManager(context)
+        companionAdapter = CompanionAdapter(
+            searchForCompanionViewModel.animals.value ?: arrayListOf(),
+            this
+        )
+        petRecyclerView = fragmentSearchForCompanionBinding
+            .petRecyclerView.apply {
+                layoutManager = viewManager
+                adapter = companionAdapter
             }
-        }
+        searchForCompanionViewModel.animals.observe(viewLifecycleOwner, {
+            companionAdapter.animals = it ?: arrayListOf()
+            companionAdapter.notifyDataSetChanged()
+        })
     }
-
 }
---
+++ CodingCompanionFinder/app/src/main/java/gri/riverjach/codingcompanionfinder/searchforcompanion/SearchForCompanionViewModel.kt
@@ -0,0 +1,50 @@
+package gri.riverjach.codingcompanionfinder.searchforcompanion
+
+import android.view.View
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import gri.riverjach.codingcompanionfinder.models.Animal
+import gri.riverjach.codingcompanionfinder.retrofit.PetFinderService
+import gri.riverjach.codingcompanionfinder.testhooks.IdlingEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.greenrobot.eventbus.EventBus
+
+class SearchForCompanionViewModel : ViewModel() {
+    val noResultsViewVisiblity: MutableLiveData<Int> = MutableLiveData<Int>()
+    val companionLocation: MutableLiveData<String> = MutableLiveData()
+
+    // 1
+    val animals: MutableLiveData<ArrayList<Animal>> =
+        MutableLiveData<ArrayList<Animal>>()
+    lateinit var accessToken: String
+    lateinit var petFinderService: PetFinderService
+
+    fun searchForCompanions() {
+        GlobalScope.launch {
+            EventBus.getDefault().post(IdlingEntity(1))
+            val searchForPetResponse = petFinderService.getAnimals(
+                accessToken,
+                location = companionLocation.value
+            )
+
+            GlobalScope.launch(Dispatchers.Main) {
+                if (searchForPetResponse.isSuccessful) {
+                    searchForPetResponse.body()?.let {
+                        animals.postValue(it.animals)
+                        if (it.animals.size > 0) {
+                            noResultsViewVisiblity.postValue(View.INVISIBLE)
+                        } else {
+                            noResultsViewVisiblity.postValue(View.VISIBLE)
+                        }
+                    }
+                } else {
+                    noResultsViewVisiblity.postValue(View.VISIBLE)
+                }
+            }
+            EventBus.getDefault().post(IdlingEntity(-1))
+        }
+    }
+
+}
